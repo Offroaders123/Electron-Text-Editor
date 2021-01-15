5a34bd337fba6e466ad46a810f650966fb6e2531
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2021-01-14 21:04:40 -0800

    Secure Renderer Process + Dependency Fixes
    Changes:
    - Disabled nodeIntegration and enableRemoteModule for BrowserWindows, then moved existing Node-exclusive code behind the contextBridge API to secure function definitions from being modifiable from within the "mainWorld" (scripts within the renderer process). This is highly recommended for rendering remote content from outside domains that you don't have control over because they would be able to execute scripts that could modify the user's system without their consent.
    - Added Electron Builder as a dependency to the project.
    - Defined various options for the build process while creating compiled executables with Electron Builder.
    
    Fixes:
    - Fixed various package.json issues where installed dependencies weren't defined, preventing the necessary dependencies from being installed when running npm install after cloning the repository.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
 app.whenReady().then(createWindow);
 
@@ -11,17 +12,29 @@ app.on("window-all-closed",() => {
 });
 
 ipcMain.on("create-window",createWindow);
+ipcMain.on("minimize-window",() => getCurrentWindow().minimize());
+ipcMain.on("maximize-window",() => getCurrentWindow().maximize());
+ipcMain.on("is-maximized-window",() => getCurrentWindow().isMaximized());
+ipcMain.on("unmaximize-window",() => getCurrentWindow().unmaximize());
+ipcMain.on("close-window",() => getCurrentWindow().close());
 
 function createWindow(){
   const window = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true
+      contextIsolation: true,
+      sandbox: true,
+      preload: path.join(__dirname,"preload.js")
     },
     frame: false,
     minWidth: 267,
     minHeight: 150,
     backgroundColor: "#333333"
   });
+  window.on("maximize",() => window.webContents.send("refresh-maximize"));
+  window.on("unmaximize",() => window.webContents.send("refresh-maximize"));
   window.loadFile("index.html");
+}
+
+function getCurrentWindow(){
+  return BrowserWindow.getFocusedWindow();
 }
---
+++ preload.js
@@ -0,0 +1,21 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("mainProcess",{/*
+  send: (channel,data) => {
+    let channels = ["toMain","create-window"];
+    if (channels.includes(channel)) ipcRenderer.send(channel,data);
+  },
+  receive: (channel,callback) => {
+    let channels = ["fromMain","create-window"];
+    if (channel == "fromMain") ipcRenderer.on(channel,(event,...args) => callback(...args));
+  },*/
+
+  send: (channel,data) => ipcRenderer.send(channel,data),
+  receive: (channel,callback) => ipcRenderer.on(channel,(event,...args) => callback(...args)),
+  createWindow: () => ipcRenderer.send("create-window"),
+  minimize: () => ipcRenderer.send("minimize-window"),
+  maximize: () => ipcRenderer.send("maximize-window"),
+  isMaximized: () => ipcRenderer.send("is-maximized-window"),
+  unmaximize: () => ipcRenderer.send("unmaximize-window"),
+  close: () => ipcRenderer.send("close-window")
+});
